4e1d7f741f97ca09adcc9afecbc099b6cb210940
taka- <takayuki.for.public@gmail.com>
2013-06-05 22:25:31 +0900

    パターンマッチに置き換え

--- src/main/scala/BWT.scala
+++ src/main/scala/BWT.scala
@@ -6,11 +6,9 @@ object BWT {
     val tf = tb.zipWithIndex.sortWith { _._1 < _._1 }.map { _._2 }
 
     def _decode(pos: Int): String = {
-      val v = tb(tf(pos))
-      if (v == '$') {
-        v.toString
-      } else {
-        v + _decode(tf(pos))
+      tb(tf(pos)) match {
+        case '$' => "$"
+        case ch => ch.toString + _decode(tf(pos))
       }
     }
     return _decode(pos)
